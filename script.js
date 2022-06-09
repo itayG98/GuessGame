74e7e5f6131f9a53f3ce9377c489324315dd1eae
Itay <91791115+itayG98@users.noreply.github.com>
2022-06-09 20:19:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const TOPVALUE = Number(20);
+
+let lottery = generateLottery();
+let checkbtn = document.querySelector('.check');
+let guessInpt = document.querySelector('.guess');
+let msg = document.querySelector('.message');
+let againbtn = document.querySelector('.again');
+let highScore = document.querySelector('.highscore');
+highScore.innerHTML = 0;
+let scoreText = document.querySelector('.score');
+let score = TOPVALUE;
+setScore();
+
+document.querySelector('.between').innerHTML = `(Between 1 and ${TOPVALUE})`;
+checkbtn.addEventListener('click', CheckClick);
+againbtn.addEventListener('click', replay);
+
+function CheckClick() {
+  if (document.querySelector('body').classList.contains('win')) {
+    replay();
+  } else if (score <= 0) {
+    msg.innerHTML = 'You`ve lost Please press replay';
+  } else if (guessInpt.value.length !== 0) {
+    checkInput();
+  } else {
+    msg.innerHTML = 'Inavilid input';
+  }
+}
+
+function replay() {
+  document.querySelector('body').classList.remove('win');
+  msg.innerHTML = 'Start guessing';
+  lottery = generateLottery();
+  score = TOPVALUE;
+  setScore();
+  document.querySelector('.number').innerHTML = '?';
+}
+
+function win() {
+  document.querySelector('body').classList.add('win');
+  msg.innerHtml = 'Correct!';
+  if (Number(highScore.innerHTML) < Number(scoreText.innerHTML)) {
+    highScore.innerHTML = scoreText.innerHTML;
+  }
+  document.querySelector('.number').innerHTML = lottery;
+}
+
+function generateLottery() {
+  return Math.trunc(Math.random() * TOPVALUE + 1);
+}
+
+function setScore() {
+  scoreText.innerHTML = `${score}`;
+}
+
+function checkInput() {
+  if (Number(guessInpt.value) === lottery && score > 0) {
+    console.log('win');
+    win();
+  } else if (score > 0) {
+    score--;
+    console.log('score --');
+    setScore();
+    if (guessInpt.value > lottery) {
+      msg.innerHTML = 'A bit lowerr';
+    } else {
+      msg.innerHTML = 'A bit higher';
+    }
+  } else {
+    msg.innerHTML = 'You`ve lost';
+  }
+}
